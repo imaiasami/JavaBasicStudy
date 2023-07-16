83df0b54a63bf048dc7cbe5593ba807aefbe12ab
Donghwan <ydonghwan299@gmail.com>
2023-07-16 21:36:16 +0900

    2023.07.16

---
+++ JavaBasicStudy/src/exam01/Test.java
@@ -0,0 +1,9 @@
+package exam01;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("콘솔 화면 출력");
+	}
+
+}
---
+++ JavaBasicStudy/src/sec01_basicsyntax/EX01_JavaBasicStructure/JavaBasicStructure.java
@@ -0,0 +1,9 @@
+package sec01_basicsyntax.EX01_JavaBasicStructure;
+
+public class JavaBasicStructure {
+	public static void main(String[] args) {
+		System.out.println("콘솔 화면에 출력");
+
+	}
+
+}
